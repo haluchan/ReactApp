75bf0ac288c59d0efb08f7c551425c2b9de40c69
halu chan <forchang0120@gmail.com>
2019-10-22 19:12:37 +0800

    使用useState hook 建立component react 16.8後支援

--- src/App.js
+++ src/App.js
@@ -1,41 +1,40 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from "./Person/Person.js";
-class App extends Component {
-  state = {
-      persons:[
-        { name: 'mac', age: 28},
-        { name: 'alex', age: 23},
-        { name: 'pei', age: 22},
-      ]
-    };
-  switchNameHandler = () => {  //要用arrow function
+const App = props => {
+  // useState 建立component 會回傳一個初始狀態值，另一個是要更新初始值的function
+  // personsState 初始設定值，setPersonsState 要更新初始值的function
+  const [ personsState, setPersonsState ] = useState({
+    persons: [
+      { name: 'mac', age: 28},
+      { name: 'alex', age: 23},
+      { name: 'pei', age: 22},
+    ],
+    otherState: 'other value'
+  });
+
+  const switchNameHandler = () => {  //要用arrow function
     // console.log('ckicked')
     //DO DO THIS: this.state.persons[0].name = 'apple'
-    this.setState({
+    setPersonsState({
       persons:[
         { name: 'apple', age: 28},
         { name: 'alex', age: 23},
         { name: 'pei', age: 30},
       ]
     })
-  }
-  render() {
+  };
     return (
       <div className='App'>
         <h1>I'm a react app</h1>
         <p>
-          <button onClick={this.switchNameHandler}>switch name</button>
+          <button onClick={ switchNameHandler }>switch name</button>
         </p>
-        <Person name={ this.state.persons[0].name } age={ this.state.persons[0].age }> hello </Person>
-        <Person name={ this.state.persons[1].name } age={ this.state.persons[1].age }/>
-        <Person name={ this.state.persons[2].name } age={ this.state.persons[2].age }/>
+        <Person name={ personsState.persons[0].name } age={ personsState.persons[0].age }> hello </Person>
+        <Person name={ personsState.persons[1].name } age={ personsState.persons[1].age }/>
+        <Person name={ personsState.persons[2].name } age={ personsState.persons[2].age }/>
       </div>
     )
-    // return (
-    //   React.createElement('div', { className:'App'}, React.createElement('h1',null ,'Hi I\'m a react app'))
-    // )
-  }
-}
+};
 
 export default App;
