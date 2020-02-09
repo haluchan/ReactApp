70596ee3b65bca5309e9507865cb01b371d726c5
halu chan <forchang0120@gmail.com>
2020-02-09 16:57:35 +0800

    udemy 46

--- src/App.js
+++ src/App.js
@@ -1,45 +1,60 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 import Person from "./Person/Person.js";
 const App = props => {
   // useState 建立component 會回傳一個初始狀態值，另一個是要更新初始值的function
   // personsState 初始設定值，setPersonsState 要更新初始值的function
   // 要更新的組成，要初始組成一樣。因為會被完全取代
-  const [ personsState, setPersonsState ] = useState({
+  const [personsState, setPersonsState] = useState({
     persons: [
-      { name: 'mac', age: 28},
-      { name: 'alex', age: 23},
-      { name: 'pei', age: 22},
-    ],
+      { name: "mac", age: 28 },
+      { name: "alex", age: 23 },
+      { name: "pei", age: 22 }
+    ]
   });
   // 多重用法，直接傳入物件或字串 setOtherState目前沒設定function
-  const [ otherState, setOtherState] = useState({otherState: 'set other state'});
+  const [otherState, setOtherState] = useState({
+    otherState: "set other state"
+  });
 
   console.log(personsState, otherState);
 
-  const switchNameHandler = () => {  //要用arrow function
+  const switchNameHandler = name => {
+    //要用arrow function
     // console.log('ckicked')
     //DO DO THIS: this.state.persons[0].name = 'apple'
     setPersonsState({
-      persons:[
-        { name: 'apple', age: 28},
-        { name: 'lux', age: 23},
-        { name: 'pei', age: 30},
+      persons: [
+        { name: name, age: 28 },
+        { name: "lux", age: 23 },
+        { name: "pei", age: 30 }
       ],
-      otherState: 'update other value'
-    })
+      otherState: "update other value"
+    });
   };
-    return (
-      <div className='App'>
-        <h1>I'm a react app</h1>
-        <p>
-          <button onClick={ switchNameHandler }>switch name</button>
-        </p>
-        <Person name={ personsState.persons[0].name } age={ personsState.persons[0].age }> hello </Person>
-        <Person name={ personsState.persons[1].name } age={ personsState.persons[1].age }/>
-        <Person name={ personsState.persons[2].name } age={ personsState.persons[2].age }/>
-      </div>
-    )
+  return (
+    <div className="App">
+      <h1>I'm a react app</h1>
+      <p>
+        <button onClick={() => switchNameHandler("Luia")}>switch name</button>
+      </p>
+      <Person
+        name={personsState.persons[0].name}
+        age={personsState.persons[0].age}
+      >
+        hello
+      </Person>
+      <Person
+        click={() => switchNameHandler("pasd")}
+        name={personsState.persons[1].name}
+        age={personsState.persons[1].age}
+      />
+      <Person
+        name={personsState.persons[2].name}
+        age={personsState.persons[2].age}
+      />
+    </div>
+  );
 };
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from "react";
 
-const person = (props) => {
-        // return <p>I'm a { props.name } and i { Math.floor( Math.random() * 30 )} years old</p>
-        return (
-          <div>
-            <p>I'm a { props.name } and I { props.age } years old</p>
-            <p>{ props.children }</p>
-          </div>
-        )// props.children 是父組件內的
+const person = props => {
+  // return <p>I'm a { props.name } and i { Math.floor( Math.random() * 30 )} years old</p>
+  return (
+    <div>
+      <p onClick={props.click}>
+        I'm a {props.name} and I {props.age} years old
+      </p>
+      <p>{props.children}</p>
+    </div>
+  ); // props.children 是父組件內的
 };
 
-export default person
+export default person;
